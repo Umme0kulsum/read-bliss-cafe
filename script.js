bfbf38bea28fd40559377dc54b5c0bfd904c0f26
Umme0kulsum <ummekulsum3107@gmail.com>
2026-02-23 14:15:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const footer = document.querySelector("footer");
+  footer.addEventListener("click", () => {
+    alert("Thanks for visiting our cafe! ☕🍰");
+  });
+});
